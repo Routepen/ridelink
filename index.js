384af73a455d138dfb3dd68e456c8b31f0ef9a5a
Porter <pmh192@gmail.com>
2017-03-09 15:19:53 -0800

    ui upgrades

--- index.js
+++ index.js
@@ -4,18 +4,25 @@ const bodyParser = require('body-parser');
 const request = require('request');
 const fs = require('fs');
 const mongoose = require('mongoose');
-const passport = require('passport');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
-const FacebookStrategy = require('passport-facebook').Strategy;
 const User = require('./models/User');
 const Route = require('./models/Route');
 const _ = require("lodash");
 const app = express();
 
+const auth = require("./auth");
+
 mongoose.Promise = require('bluebird');
 var conn = mongoose.createConnection('ds161169.mlab.com:61169/heroku_9170g7ps');
-mongoose.connect('mongodb://victorcheng:victor97@ds161169.mlab.com:61169/heroku_9170g7ps');
+mongoose.connect('mongodb://victorcheng:victor97@ds161169.mlab.com:61169/heroku_9170g7ps',  {
+  server: {
+    socketOptions: {
+      socketTimeoutMS: 0,
+      connectTimeoutMS: 0
+    }
+  }
+});
 var db = mongoose.connection;
 
 /*
@@ -49,54 +56,9 @@ app.use(cookieParser());
 
 // parse application/json
 app.use(bodyParser.json());
-app.use(passport.initialize());
-app.use(passport.session());
 
-passport.serializeUser(function(user, done) {
-	done(null, user._id);
-});
+auth.setUpAuth(app);
 
-passport.deserializeUser(function(id, done) {
-	User.findById(id, function(err, user) {
-		done(err, user);
-	});
-});
-
-
-passport.use(new FacebookStrategy({
-		clientID: "1122786494515943",
-		clientSecret: "f6cd89a5fe00867021469477156f95a6",
-		callbackURL: "http://localhost:5000/auth/facebook/callback",
-		profileFields: ['id', 'first_name', 'last_name', 'photos', 'email', 'gender', 'link']
-	},
-	function(accessToken, refreshToken, profile, done) {
-		process.nextTick(function(){
-			User.findOne({ 'facebook.id': profile.id}, function (err, user) {
-				if (err)
-					return done(err);
-				else if (user) {
-					return done(null, user);
-				} else {
-					var newUser = new User();
-					newUser.facebook.id = profile.id;
-					newUser.facebook.token = accessToken;
-					newUser.facebook.name = profile.name.givenName + ' ' + profile.name.familyName;
-					newUser.facebook.email = (_.get(profile, 'emails[0].value', '')).toLowerCase();
-					newUser.facebook.photos = profile.photos;
-					newUser.facebook.gender = profile.gender;
-					newUser.facebook.link = profile.profileUrl;
-
-					newUser.save(function(err) {
-						if (err)
-							throw err;
-						return done(null, newUser);
-					});
-				}
-				return;
-			});
-		});
-	}
-));
 
 app.get('/', function (req, res) {
 	var data = {
@@ -395,35 +357,6 @@ function random(len) {
 
 app.get('/profile', function(req,res){
 	res.send("hello");
-});
-
-app.get('/auth/logout', function(req, res) {
-	req.logout();
-	res.redirect("/");
-});
-
-app.get('/auth/facebook', function(req,res,next){
-	console.log('/auth/facebook/callback?redirect=' + encodeURIComponent(req.query.redirect));
-	passport.authenticate(
-		'facebook',
-		{callbackURL: '/auth/facebook/callback?redirect=' + encodeURIComponent(req.query.redirect) },
-		{scope:["public_profile,email"]}
-	)(req,res,next);
-});
-
-app.get('/auth/facebook/callback/', function(req,res,next) {
-	console.log('/auth/facebook/callback?redirect=' + encodeURIComponent(req.query.redirect));
-	passport.authenticate(
-		'facebook',
-		{
-			callbackURL: '/auth/facebook/callback?redirect=' + encodeURIComponent(req.query.redirect),
-			successRedirect: req.query.redirect,
-			failureRedirect: '/'
-		}
-	)(req,res,next);
-});
-app.get('/logout', function(req,res){
-
 });
 
 app.get('/rider/', function(req,res){
