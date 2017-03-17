d4900bc7519ec6cd3b446f974d79d6daf7a1bf4b
highiqkid <victor_cheng@yahoo.com>
2017-03-17 00:07:58 -0700

    some minor edits

--- mail.js
+++ mail.js
@@ -224,7 +224,7 @@ function sendMail(options) {
       "Routepen Team";
 
       html = "<span>Welcome to Routepen!,</span><br/><br/>"+
-      "<span>You're now apart of the team. We're here for you to help you make some serious money without worrying about last minute bailing! Now all you have to do is to post on the Fb Rideshare post with your link included. Keep in mind your route is only useful if you share it!</span><br/><br/>"+
+      "<span>Congrats on creating a route! We're here for you to help you make some serious money without worrying about last minute bailing! Now all you have to do is to post on the Fb Rideshare post with your link included. Keep in mind your route is only useful if you share it!</span><br/><br/>"+
       "<span>Then just sit back and wait for riders to enter their dropoff locations on the map and confirm the ones you like. We'll even help you by giving you an email every time a rider signs up. (Excess riders will automatically be added to the waitlist) :)</span><br/><br/>"+
       "<span>Best,</span><br/>" +
       "<span>Routepen Team</span>";
