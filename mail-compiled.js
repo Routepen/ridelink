98bd0b0df721a1c059d82ee7065369d0762e40b0
highiqkid <victor_cheng@yahoo.com>
2017-03-18 21:24:53 -0700

    email changes

--- mail-compiled.js
+++ mail-compiled.js
@@ -137,7 +137,7 @@ function sendMail(options) {
     }
     if (options.notifyDriver.routeCreated) {
       subject = "Route Created";
-      text = "Welcome to Routepen!, \n\n" + "Congrats on creating a route! Routepen is here to help you make some serious money without worrying about last minute bailing! Now all you have to do is to post on the Facebook Rideshare page with your link included. Keep in mind your route is only useful if you share it!\n\n" + "Then just sit back and wait for riders to enter their dropoff locations on the map and confirm the ones you like. We'll even help you by giving you an email every time a rider signs up. (Excess riders will automatically be added to the waitlist) :)\n\n" + "Best, \n" + "Routepen Team";
+      text = "Welcome to Routepen! \n\n" + "Congrats on creating a route! Routepen is here to make Rideshare on Facebook much better! Now all you have to do is post on the Facebook Rideshare page with your link included. Keep in mind your route is only useful if you share it!\n\n" + "Then just sit back and wait for riders to enter their dropoff locations on the map and confirm the ones you like. We'll even help you by giving you an email every time a rider signs up. (Excess riders will automatically be added to the waitlist) :) Hope that he\n\n" + "Best, \n" + "Routepen Team";
       /*
       Old text
       text = "Welcome to Routepen!,\n\n"+
@@ -147,7 +147,7 @@ function sendMail(options) {
       "Routepen Team";
       */
 
-      html = "<span>Welcome to Routepen!,</span><br/><br/>" + "<span>Congrats on creating a route! Routepen is here to help you make some serious money without worrying about last minute bailing! Now all you have to do is to post on the Facebook Rideshare page with your link included. Keep in mind your route is only useful if you share it!</span><br/><br/>" + "<span>Then just sit back and wait for riders to enter their dropoff locations on the map and confirm the ones you like. We'll even help you by giving you an email every time a rider signs up. (Excess riders will automatically be added to the waitlist) :)</span><br/><br/>" + "<span>Best,</span><br/>" + "<span>Routepen Team</span>";
+      html = "<span>Welcome to Routepen!</span><br/><br/>" + "<span>Congrats on creating a route! Routepen is here to help you make some serious money without worrying about last minute bailing! Now all you have to do is to post on the Facebook Rideshare page with your link included. Keep in mind your route is only useful if you share it!</span><br/><br/>" + "<span>Then just sit back and wait for riders to enter their dropoff locations on the map and confirm the ones you like. We'll even help you by giving you an email every time a rider signs up. (Excess riders will automatically be added to the waitlist) :)</span><br/><br/>" + "<span>Best,</span><br/>" + "<span>Routepen Team</span>";
     }
   }
 
