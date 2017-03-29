023afe813ef06bd2d0c17c67585d26afe6eafe96
Porter <pmh192@gmail.com>
2017-03-28 22:59:53 -0700

    started algo

--- googlemaps.js
+++ googlemaps.js
@@ -28,11 +28,33 @@ var request = {
     travelMode: 'DRIVING'
 };
 
+var northMost = 41.998373, westMost = -124.415248,
+  southMost = 32.534277, eastMost = -114.140199;
+
+var count1 = 0, count2 = 0, inc = .416;// .416 degrees of lat/lng is about 25 miles
+// inc can be adjusted as needed
+for (var lng = westMost; lng < eastMost; lng += inc) {
+  count1++;
+  count2 = 0;
+  for (var lat = northMost; lat > southMost; lat -= inc) {
+    count2++;
+  }
+}
+console.log(count1, count2);
+
+var compartments = new Array(count1*count2);
+
+
 gmAPI.directions(request, function(err, result){
-    console.log(polyline.decode(result.routes[0].overview_polyline.points));
-    // for each element in the polyline array figure out if it is on the way or to your location
+    var points = polyline.decode(result.routes[0].overview_polyline.points);
+    points.forEach((p, i) => {
+      var x = parseInt((p[0] - southMost)/inc), y = parseInt((p[1] - westMost)/inc)
+      console.log(x, y);
+      var index = x + y*count1;
+
+      var score = compartments[index] || 0;
+      score += i / points.length;
+      compartments[index] = score;
+
+    })
 });
-
-
-
-
