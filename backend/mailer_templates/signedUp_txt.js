f3c713b2ba2fb088eca0d25b010a908dcef0040a
dannycho7 <dannycho7@gmail.com>
2017-04-26 23:45:03 -0700

    Signed up email templates

---
+++ backend/mailer_templates/signedUp_txt.js
@@ -0,0 +1,10 @@
+module.exports = function(recipientName, driversName){
+  return "Hi " + recipientName + ",\n\n" +
+    "Welcome to Routepen! HTML smirk with sunglasses emoji\n\n" +
+    "Congrats on signing up with " + driversName + "'s ride through Routepen! We'll give you another follow up email once " + driversName + " has confirmed you as a passenger they want to take. We'll also keep you posted on any change of plans for you!\n\n" +
+    "<b> New Rider Feature </b>\n\n" +
+    "If you find yourself having a hard time finding rides, we're rolling out a new Instant notifications feature. We give you a notification every time there is a ride on the way or to where you're trying to go. If you find yourself having a tough time finding rides, join the waitlist and you'll be the first to know about it. We only have 300 spots, so sign up quick!\n\n" +
+    "In the meantime, find other drivers through the Facebook Groups (Bay Area, SoCal, and the Regular). Pin your dropoff location. And let us handle the rest for you! :)\n\n" +
+    "Best,\n" +
+    "Routepen Team";
+}
