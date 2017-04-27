ac18d4eda7d376c605200c306fa10dc2283c7fba
dannycho7 <dannycho7@gmail.com>
2017-04-26 23:44:50 -0700

    should be picked up template

---
+++ backend/mailer_templates/shouldBePickingUp_txt.js
@@ -0,0 +1,10 @@
+module.exports = function(driversName){
+  return "Hi " + driversName + ",\n\n" +
+  "Hope the ride is going well! Now's the time to let us know if you've taken the riders and more importantly how to get your money. Confirm here if you've picked them up and assuming no riders deny you've taken them in the next day, we'll send you the money through venmo. Just give it a day!\n\n" +
+  "If any of the riders deny, we will look into it and give you a follow up email with details.\n\n" +
+  "Otherwise if you weren't able to show up, deny here and we'll refund the riders!\n\n" +
+  "Thanks for your honesty! To assure quality in this exclusive community, we unfortunately do have to take further action if caught lying, but rest assured I know you're not going to be one of them :)\n\n" +
+  "Have a wonderful day and let us know your thoughts on RoutePen here if you have any questions or concerns!\n\n" +
+  "Best,\n" +
+  "RoutePen Team";
+}
