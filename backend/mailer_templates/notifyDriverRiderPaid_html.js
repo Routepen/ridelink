53507c26b4757ab0251c9557008645a794034783
dannycho7 <dannycho7@gmail.com>
2017-04-26 23:43:58 -0700

    notifyDriver templates

---
+++ backend/mailer_templates/notifyDriverRiderPaid_html.js
@@ -0,0 +1,7 @@
+module.exports = function(driversName, riderName, fbLink){
+  return "Hi " + driversName + ",\n\n" +
+  "We have some awesome news. " + riderName + " has paid his initial deposit and they're locked in for the ride. On the day of the ride, we'll send you an email confirming that you have taken them and you'll get your money then!\n\n" +
+  "If interested, visit " + riderName + "'s Facebook profile on your map here and send them a quick message with details of the ride.\n\n" +
+  "Best,\n" +
+  "Routepen Team";
+}
---
+++ backend/mailer_templates/notifyDriverRouteCreated_txt.js
@@ -0,0 +1,7 @@
+module.exports = function(){
+  return "Welcome to Routepen! \n\n" +
+  "Congrats on creating a route! Routepen is here to make Rideshare on Facebook much better! Now all you have to do is post on the Facebook Rideshare page with your link included. Keep in mind your route is only useful if you share it!\n\n"+
+  "Then just sit back and wait for riders to enter their dropoff locations on the map and confirm the ones you like. We'll even help you by giving you an email every time a rider signs up. (Excess riders will automatically be added to the waitlist) :) Hope that helps!\n\n"+
+  "Best, \n" +
+  "Routepen Team";
+}
