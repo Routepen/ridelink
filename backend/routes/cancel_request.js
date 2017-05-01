ceaa9034118970e2ce02368037e7ae4c9b3a7296
highiqkid <victor_cheng@yahoo.com>
2017-05-01 02:51:09 -0700

    Changed a lot of shit

--- backend/routes/cancel_request.js
+++ backend/routes/cancel_request.js
@@ -21,25 +21,52 @@ module.exports = function(app, Route, User) {
       if (!removed) {
         return res.redirect("/route?id=" + (route.shortId || route._id) + "&error=2");
       }
+      /*
       var dropOffs = route.dropOffs || {};
-      delete dropOffs[req.user._id]
+      delete dropOffs[req.user._id];
       route.dropOffs = dropOffs;
+      */
+      delete route.dropOffs[req.user._id];
+      delete route.pickUps[req.user._id];
 
+/*
+      var temppickUps = route.pickUps || {};
+      delete temppickUps[req.user._id];
+      route.pickUps = temppickUps;
+*/
       route.riderStatus = route.riderStatus || {};
       if (route.riderStatus[removingId]) {
         delete route.riderStatus[removingId];
       }
       route.markModified('dropOffs');
+      route.markModified('pickUps');
       route.markModified('riderStatus');
 
       route.save(function(err) {
         if (err) { console.log(err); return res.end(err.toString()); }
-
         res.end("success");
       })
     });
-    User.findById(req.user._id, function(err, riderUser) {
-      console.log('rider', riderUser);
+
+
+    User.findById(removingId, function(err, riderUser) {
+        var index = -1;
+        for(var i = 0; i < riderUser.routes.length; i++){
+            if(riderUser.routes[i] == req.body.routeId){
+                index = i;
+            }
+        }
+        if(index != -1){
+          riderUser.routes.splice(index, 1);
+         }
+
+         riderUser.markModified('routes');
+
+        riderUser.save(function(err){
+          if (err) { console.log(err); return res.end(err.toString()); }
+          res.end("deleted user's route entry");
+        });
     });
+
   });
 }
---
+++ backend/routes/remove_rider_entirely.js
@@ -0,0 +1,97 @@
+module.exports = function(app, Route, User) {
+
+app.post('/route/removeriderentirely', function(req, res) {
+  //TODO if it is a cancel request do that in mass, if it is a confirmedRider do that in mass
+    if (!req.user) {
+      return res.end("please log in ");
+    }
+
+    var removingId = req.body.userId;
+
+    var data = {
+        '_id':{ $in: req.body.routeIds}
+    }
+
+    Route.find(data, function(err, routes) {
+
+      for(var i = 0; i < routes.length; i++){
+        if ( !((routes[i].riders.indexOf(req.user._id.toString()) > -1) || (routes[i].confirmedRiders.indexOf(req.user._id.toString())) > -1)) {
+          console.log("hacking");
+          return res.end("failure");
+        }
+      }
+
+      for(var k = 0; k < routes.length; k++){
+        for (var i = 0; i < routes[k].confirmedRiders.length; i++) {
+          var rider = routes[k].confirmedRiders[i];
+          if (rider.toString() == removingId) {
+            console.log("removing confirmed rider on routes", routes[k]._id);
+            routes[i].confirmedRiders.splice(i, 1);
+            break;
+          }
+        }
+      }
+
+      for(var k = 0; k < routes.length; k++){
+        for (var i = 0; i < routes[k].riders.length; i++) {
+          var rider = routes[k].riders[i];
+          if (rider.toString() == removingId) {
+            console.log("removing regular rider on routes", routes[k]._id);
+            routes[k].riders.splice(i, 1);
+            break;
+          }
+        }
+      }
+
+      for(var i = 0; i < routes.length; i++){
+        delete routes[i].dropOffs[req.user._id];
+        delete routes[i].pickUps[req.user._id];
+      }
+
+      for(var i = 0; i < routes.length; i++){
+          if (routes[i].riderStatus[removingId]) {
+            delete routes[i].riderStatus[removingId];
+          }
+      }
+
+      for(var i = 0; i < routes.length; i++){
+        routes[i].markModified('dropOffs');
+        routes[i].markModified('pickUps');
+        routes[i].markModified('riderStatus');
+      }
+
+      for(var i = 0; i < routes.length; i++){
+          routes[i].save(function(err){
+            if (err) { console.log(err); return res.end(err.toString()); }
+
+            res.end("success");
+          });
+      }
+    });
+
+    User.findById(removingId, function(err, riderUser) {
+        var index = riderUser.routes.length;
+        for(var i = 0; i < index; i++){
+          console.log(typeof riderUser.routes[i], riderUser.routes[i]);
+            console.log("BITCH" , req.body.routeIds.indexOf(riderUser.routes[i].toString()) > -1);
+            if(req.body.routeIds.indexOf(riderUser.routes[i].toString()) > -1){
+              riderUser.routes.splice(i, 1);
+              index--;
+              i--;
+            }
+        }
+
+        //riderUser is not actually changing to its original array
+
+        console.log("FROM USER", riderUser.routes);
+        riderUser.markModified('routes');
+
+        riderUser.save(function(err){
+          if (err) { console.log(err); return res.end(err.toString()); }
+          res.end("deleted user's route entry");
+        });
+
+    });
+
+  });
+}
