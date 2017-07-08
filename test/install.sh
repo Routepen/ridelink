0bfbd00e4843ff789e923f86bb9c0164506cbef4
Porter <pmh192@gmail.com>
2017-07-07 19:46:10 -0700

    final changes

--- test/install.sh
+++ test/install.sh
@@ -10,7 +10,6 @@ echo
 curl -O http://selenium-release.storage.googleapis.com/3.0/selenium-server-standalone-3.0.1.jar
 
 echo
-echo "You're going to have to install geckodriver yourself, as it is platform dependent."
-echo "You'll have to downlaod the correct executable from https://github.com/mozilla/geckodriver/releases"
-echo "And move it to test/geckodriver so that when you run selenium-server.sh it can find the driver"
-
+echo "You're going to have to install chrome yourself, as it is platform dependent."
+echo "Get chrome driver version 2.25"
+echo "And move it to test/chromedriver so that when you run selenium-server.sh it can find the driver"
